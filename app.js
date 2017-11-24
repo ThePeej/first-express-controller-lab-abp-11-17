11991cc1edd7727ca898e2dc69070c8bc71db6ca
PJ Frias <peejfrias@gmail.com>
2017-11-24 16:15:27 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 var express = require('express')
 var app = express()
 
+app.get('/', function(req,resp){
+  
+})
 
+app.get('/about', function(req,resp){
+  
+})
+
+app.get('/contact', function(req,resp){
+  
+})
 
 module.exports = app
 
