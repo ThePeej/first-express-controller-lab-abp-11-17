90dafa9b8d15b3d219819df8c49fe8ce1dbb4e63
PJ Frias <peejfrias@gmail.com>
2017-11-24 16:31:14 +0000

    automatically backed up by learn

--- controllers/SiteController.js
+++ controllers/SiteController.js
@@ -1,11 +1,4 @@
-app.get('/', function(req,resp){
-  resp.render('index')
-})
 
-app.get('/about', function(req,resp){
-  resp.render('about')
-})
-
-app.get('/contact', function(req,resp){
-  resp.render('contact')
-})
+function index(req,resp){
+   resp.render('index')
+ }
