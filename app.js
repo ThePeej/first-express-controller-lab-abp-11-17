757c54a9bf69978daffee1fc5e0e2d0d62043ab8
PJ Frias <peejfrias@gmail.com>
2017-11-24 16:22:01 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ var express = require('express')
 var app = express()
 
 app.get('/', function(req,resp){
-  resp.render(site/index)
+  resp.render('index')
 })
 
 app.get('/about', function(req,resp){
-  resp.render(site/index)
+  resp.render('about')
 })
 
 app.get('/contact', function(req,resp){
-  resp.render(site/contact)
+  resp.render('contact')
 })
 
 module.exports = app
