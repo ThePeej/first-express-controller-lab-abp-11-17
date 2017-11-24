2d3f76ab6a243b27e92daf7b5859e66be8f60c40
PJ Frias <peejfrias@gmail.com>
2017-11-24 16:33:08 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 var express = require('express')
 var app = express()
+var SiteController = require('siteController')
 
+app.get('/', index)
 
+app.get('/about', function(req,resp){
+  resp.render('about')
+})
+
+app.get('/contact', function(req,resp){
+  resp.render('contact')
+})
 
 module.exports = app
